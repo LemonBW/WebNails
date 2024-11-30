0541094742ce86e66cce1e6d9a00740a9faed222
LemonBW <139037265+LemonBW@users.noreply.github.com>
2024-11-30 23:05:13 +0500

    Connecting and testing the Tailwind CSS Library

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html', 'services.html', 'courses.html', 'contacts.html', 'about.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
